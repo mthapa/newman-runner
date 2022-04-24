e40dfe91a798a6867740567cab1a8d1855344856
Madan Thapa <thapamadan@gmail.com>
2022-04-24 16:43:58 -0500

    Initial Add

---
+++ index.js
@@ -0,0 +1,11 @@
+const newman = require('newman'); // require newman in your project
+
+console.log('collection run start!');
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+    collection: require('./postman_collection.json'),
+    reporters: 'cli'
+}, function (err) {
+	if (err) { throw err; }
+    console.log('collection run complete!');
+});
