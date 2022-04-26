4d712934558c814d2e25176e47a1a4332414cdaa
Madan Thapa <thapamadan@gmail.com>
2022-04-25 20:54:34 -0500

    Move Index.js  inside src

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const newman = require('newman'); // require newman in your project
 console.log('collection run start!');
 // call newman.run to pass `options` object and wait for callback
 newman.run({
-    collection: require('./postman_collection.json'),
+    collection: require('../postman_collection.json'),
     reporters: 'cli'
 }, function (err) {
 	if (err) { throw err; }
